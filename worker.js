254f4868bf5aba9ce9cb1d3621d12f2d1e839589
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-25 02:08:24 +0330

    A domain was added

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
+ * Welcome to Cloudflare Workers!This is your first worker.
  *
  * - Run "npm run dev" in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
